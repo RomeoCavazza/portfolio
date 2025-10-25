db07a632473927335c5b3499b358c3a0e98267b8
Namtar-afk <romeo.cavazza@gmail.com>
2025-10-26 00:32:20 +0200

    feat: add contact info to footer with animations
    - Remove email from landing page
    - Add professional contact section in footer
    - Display email and phone with icons
    - Add ScrollReveal animations for footer elements
    - Clean and subtle presentation

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,5 +47,7 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200});
+sr.reveal('.footer__contact',{delay: 600, origin: 'bottom'}); 
+sr.reveal('.footer__social, .footer__copy',{delay: 400, origin: 'bottom'}); 
             
